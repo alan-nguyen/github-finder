eaec3dc2e1b2d1009c5fff6b1edeb34b1158c67b
Alan Nguyen <tuan8101@gmail.com>
2020-03-08 22:30:46 -0600

    Create getUser with async

--- github.js
+++ github.js
@@ -1,7 +1,16 @@
 class GitHub {
   constructor() {
-    this.client_id = 'bbcf552bbffbdfeb86f3';
-    this.client_secret = 'f747b65a4e60e3f8b8549dd4b332bcbb3f506052
-    ';
+    this.client_id = "bbcf552bbffbdfeb86f3";
+    this.client_secret = "f747b65a4e60e3f8b8549dd4b332bcbb3f506052";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = awati profileResponse.json();
+
+    return {
+      profile
+    }
   }
 }
