51c4c6b450bca475cdbcd903ad046bc0b54c7dbf
Alan Nguyen <tuan8101@gmail.com>
2020-03-12 21:33:10 -0600

    Insert alert

--- ui.js
+++ ui.js
@@ -30,7 +30,23 @@ class UI {
       <div id="repos"></div>
     `;
   }
+  // Show alert message
+  showAlert() {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+  }
 
+  // Clear Profile
   clearProfile() {
     this.profile.innerHTML = '';
   }
